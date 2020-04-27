937338c80cccede62791b9a46a44f60b36da87b3
Bakugou808 <janu.m.sung@gmail.com>
2020-04-26 17:19:08 -0700

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function testFunc() {
 }
 
 function superbowlWin(arr){
-  let result = arr.find(record => record.result === "W")
+  let result = arr.find(record => record["result"] === "W")
   return result ? result.year : undefined
 }
