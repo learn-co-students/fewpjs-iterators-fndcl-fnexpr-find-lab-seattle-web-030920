75491a4dd8d8a7a6c1f577c9f96e89e477f41253
dizhengcindy <dizhengcindy@gmail.com>
2020-04-30 13:04:16 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(record){
+  let result = record.find(obj => obj.result==="W")
+  return !!result ? result.year:undefined
+}
