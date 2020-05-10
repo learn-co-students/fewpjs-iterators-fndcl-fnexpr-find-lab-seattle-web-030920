8e2fa923c2d1cb66c3819a9d3352bb794aca73b9
Theouty <aman.david.michael@gmail.com>
2020-05-10 00:42:56 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let sObject = array.find(element => element.result === "W")
+  return sObject ? sObject.year : undefined
+}
